57ad2fadabd3a779936a6000d3f66fe82a625f6e
YarinBS <yarin10121@gmail.com>
2020-06-07 23:32:11 +0300

    Javadocs in MarkovZero

--- Part2/MarkovZero.java
+++ Part2/MarkovZero.java
@@ -1,30 +1,42 @@
 import java.util.Random;
 
-public class MarkovZero extends AbstractMarkovModel {
+public class MarkovZero extends AbstractMarkovModel { /** subclass of AbstractMarkovModel, contains the MarkovZero arguments and methods */
     private String myText;
 	private Random myRandom;
 	
-	public MarkovZero() {
+	public MarkovZero() { /** Class Constructor */
 		super(0);
 		myRandom = new Random();
 	}
 
 	public void setSeed(int seed){
+		/** @param seed - a number that sets myRandom */
 		myRandom = new Random(seed);
 	}
 
 	@Override
 	public String toString() {
+		/**
+		 * @return a String containing the order of the Markov object
+		 */
 		return "MarkovModel of order " + this.orderOfMarkov;
 	}
 
 	@Override
 	public void setTraining(String s){
+		/** removes whitepaces from the String 's'
+		 * @param s - a text from the given file
+		 */
 		myText = s.trim();
 	}
 
 	@Override
 	public String getRandomText(int numChars){
+		/** given an integer 'numChars', returns a String of text with numChars characters
+		 * @param numChars - set the number of characters that will be printed in each paragraph
+		 * @return a String containing the scrambled text
+		 */
+
 		if (myText == null){
 			return "";
 		}
